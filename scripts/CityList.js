76933f33068bacd69f5889f97fa3741ac952f058
turner lynch <turnerjoylynch@gmail.com>
2021-11-15 20:17:50 -0600

    book 3 dog walker assignment

---
+++ scripts/CityList.js
@@ -0,0 +1,16 @@
+import { getWalkers } from "./database.js"
+
+const walkers = getWalkers()
+
+
+export const CityList = () => {
+    let citiesHTML = "<ol>"
+
+    for (const walker of walkers) {
+        citiesHTML += `<li>${walker.city}</li>`
+    }
+
+    citiesHTML += "</ol>"
+    return citiesHTML
+}
+
